c2d17b9a4b449ac3de7b7a878869011a2d655af8
fwg <adf0001@163.com>
2022-07-11 21:25:57 +0800

    small fix: level-3

--- level-3.js
+++ level-3.js
@@ -4,11 +4,11 @@
 Object.assign(exports,
 	require("./level-2.js"),
 
+	require("./lib/node-info.js"),
+	require("./lib/dataset.js"),
+	
 	require("./lib/add.js"),
 	require("./lib/insert.js"),
 	require("./lib/remove.js"),
 	require("./lib/update.js"),
-	require("./lib/node-info.js"),
-
-	require("./lib/dataset.js"),
 );
